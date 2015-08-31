80e25386c80527a0426ff0dfb355dbe4fbf1f2e3
Mushex Antaranian <mushex@antaranian.com>
2015-08-31 16:03:57 +0400

    Add dummy tests with Mocha

---
+++ test/openinghours.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+var OpeningHours = require('../lib/openinghours.js');
+
+describe('OpeningHours', function() {
+    it('should exist', function() {
+        assert.ok(OpeningHours);
+    });
+});
