18767224e51f19ac70c5e22b0d07b6fab50e9ba6
Mushex Antaranian <mushex@antaranian.com>
2015-09-02 13:33:30 +0400

    Define initial components' structure
    In a manner of:
    
      OpeningHours
      - [Day]
        - HoursCollection
          - [Hours]

--- test/openinghours.js
+++ test/openinghours.js
@@ -2,8 +2,78 @@ var assert = require('assert');
 
 var OpeningHours = require('../lib/openinghours.js');
 
+var data;
+
+data = [
+    {
+        dayOfWeek: 'Monday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }, {
+            'opens': '15։00',
+            'closes': '21:00'
+        }],
+    }, {
+        dayOfWeek: 'Tuesday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }, {
+            'opens': '15։00',
+            'closes': '21:00'
+        }],
+    }, {
+        dayOfWeek: 'Wednesday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }, {
+            'opens': '15։00',
+            'closes': '21:00'
+        }],
+    }, {
+        dayOfWeek: 'Thursday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }, {
+            'opens': '15։00',
+            'closes': '21:00'
+        }],
+    }, {
+        dayOfWeek: 'Friday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }, {
+            'opens': '15։00',
+            'closes': '21:00'
+        }],
+    }, {
+        dayOfWeek: 'Saturday',
+        hours: [{
+            'opens': '10:00',
+            'closes': '14:00'
+        }]
+    }
+];
+
 describe('OpeningHours', function() {
+    var openinghours;
+
     it('should exist', function() {
         assert.ok(OpeningHours);
+    });
+
+    it('should initialize correctly', function(){
+         openinghours = new OpeningHours(data);
+
+         var mondayHours = [
+                { opens: '10:00', closes: '14:00' },
+                { opens: '15։00', closes: '21:00' }
+            ];
+
+         assert.deepEqual(openinghours.get('Monday').hours.serialize(), mondayHours);
     });
 });
