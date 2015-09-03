b63f256536e0cccebab9b204fa4ef033aab996fe
Mushex Antaranian <mushex@antaranian.com>
2015-09-03 15:58:22 +0400

    Add raw text parsing
    Each component captures data it needs
    (and captured raw data goes to it's nodes)
    
    ---
    
               /---\---/---\ /---\---/֊֊֊\
      'Monday: 10:00 - 14:00 15:00 - 21:00'
       |____|  |___________| |___________|
        |     \___________________________/
        |	    |
        |	    |	Day {}
        |       |
        |       +->   dayOfWeek
        |
        +--------->   hours []
    	   	  - Hours { opens, closes }

--- test/openinghours.js
+++ test/openinghours.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
 
+var fs = require('fs');
+
 var OpeningHours = require('../lib/openinghours.js');
 
 var data = {
-        sane: require('./assets/sane.json')
+        sane    : require('./assets/sane.json'),
+        longEng : fs.readFileSync(__dirname  + '/assets/long-english.txt', 'utf8')
     };
 
 describe('OpeningHours', function() {
@@ -15,6 +18,18 @@ describe('OpeningHours', function() {
 
     it('should serialize correctly', function(){
          openinghours = new OpeningHours(data.sane, { parse: true });
+
+         var result = 'Mo-Fr 10:00-14:00 15:00-21:00; Sa 10:00-14:00';
+
+         assert.deepEqual(openinghours.serialize(), result);
+    });
+});
+
+describe('Parse', function(){
+    var openinghours;
+
+    it('should accept raw test', function () {
+         openinghours = new OpeningHours(data.longEng, { parse: true });
 
          var result = 'Mo-Fr 10:00-14:00 15:00-21:00; Sa 10:00-14:00';
 
