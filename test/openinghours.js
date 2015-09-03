69d16a7e50d7b70d194b759a3fbebd875f3c9ca7
Mushex Antaranian <mushex@antaranian.com>
2015-09-03 12:39:51 +0400

    Move test assets from code to separate files

--- test/openinghours.js
+++ test/openinghours.js
@@ -2,62 +2,9 @@ var assert = require('assert');
 
 var OpeningHours = require('../lib/openinghours.js');
 
-var data;
-
-data = [
-    {
-        dayOfWeek: 'Monday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }, {
-            'opens': '15:00',
-            'closes': '21:00'
-        }],
-    }, {
-        dayOfWeek: 'Tuesday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }, {
-            'opens': '15:00',
-            'closes': '21:00'
-        }],
-    }, {
-        dayOfWeek: 'Wednesday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }, {
-            'opens': '15:00',
-            'closes': '21:00'
-        }],
-    }, {
-        dayOfWeek: 'Thursday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }, {
-            'opens': '15:00',
-            'closes': '21:00'
-        }],
-    }, {
-        dayOfWeek: 'Friday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }, {
-            'opens': '15:00',
-            'closes': '21:00'
-        }],
-    }, {
-        dayOfWeek: 'Saturday',
-        hours: [{
-            'opens': '10:00',
-            'closes': '14:00'
-        }]
-    }
-];
+var data = {
+        sane: require('./assets/sane.json')
+    };
 
 describe('OpeningHours', function() {
     var openinghours;
@@ -67,7 +14,7 @@ describe('OpeningHours', function() {
     });
 
     it('should serialize correctly', function(){
-         openinghours = new OpeningHours(data, { parse: true });
+         openinghours = new OpeningHours(data.sane, { parse: true });
 
          var result = 'Mo-Fr 10:00-14:00 15:00-21:00; Sa 10:00-14:00';
 
