a67d6f114117d27a4c9afe106845ad256288ba2c
Mushex Antaranian <mushex@antaranian.com>
2015-09-10 15:14:35 +0400

    Make `parse` more external
    Configure parsing process by options as second argument

--- test/openinghours.js
+++ test/openinghours.js
@@ -5,9 +5,6 @@ var fs = require('fs');
 var OpeningHours = require('../lib/openinghours.js');
 
 
-var VOCABULARY = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
-
-
 var data = {
         sane    : require('./assets/sane.json'),
         longEng : fs.readFileSync(__dirname  + '/assets/long-english.txt', 'utf8')
@@ -33,7 +30,12 @@ describe('Parse', function(){
     var openinghours;
 
     it('should accept raw test', function () {
-         openinghours = new OpeningHours(data.longEng, { parse: true, vocabulary: VOCABULARY });
+         openinghours = new OpeningHours(data.longEng, {
+            parse: true,
+            vocabulary: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+            dayMatch: /(\w+)\.:(.+)/,
+            hoursSplit: /,\s/g
+        });
 
          var result = 'Mo-Fr 10:00-14:00 15:00-21:00; Sa 10:00-14:00';
 
